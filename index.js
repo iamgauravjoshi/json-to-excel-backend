7038e1250845958b248206b04c97f848d0b5efe6
okruti-gjoshi <gjoshi@okruti.com>
2024-12-27 16:28:57 +0530

    updated backend code

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.post("/upload", upload.single("file"), (req, res) => {
